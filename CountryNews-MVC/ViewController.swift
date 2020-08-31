ef0c802c2996c2ced1c1c15656f502e4acf88ef7
Christian Tanputra <christian_tanputra@icloud.com>
2020-08-31 11:37:00 +0700

    Initial Commit

---
+++ CountryNews-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryNews-MVC
+//
+//  Created by Christian Tanputra on 31/08/20.
+//  Copyright © 2020 Christian Tanputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
